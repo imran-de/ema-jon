4aca17d5a0112da79a09ef71e0146f1adc5d70ed
imran-de <stimran4405@gmail.com>
2021-09-24 09:38:49 +0600

    added cart option and search functinality

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -5,8 +5,10 @@ const Cart = (props) => {
     //odd way
     /* const totalReducer = (previous, product) => previous + product.price;
     const total = cart.reduce(totalReducer, 0); */
-    //smart way
-    const total = cart.reduce((previous, product) => previous + product.price, 0)
+    //smart way    
+    const total = cart.reduce((previous, product) => previous + (product.price) * (!product.quantity ? 1 : product.quantity), 0)
+    //(!product.quantity ? 1 : product.quantity) use condition for avoind NaN error
+    const totalQuantity = cart.reduce((previous, product) => previous + (!product.quantity ? 1 : product.quantity), 0);
     const shipping = cart.reduce((previous, product) => previous + product.shipping, 0)
     /*  let total = 0;
      for (const product of cart) {
@@ -19,7 +21,7 @@ const Cart = (props) => {
     return (
         <div className='cart-summary'>
             <h3>Order Summary</h3>
-            <p>Order Item: {props.cart.length}</p>
+            <p>Order Item: {totalQuantity}</p>
             <p>Total: ${total.toFixed(2)}</p>
             <p>Shipping & Handling: {shipping.toFixed(2)}</p>
             <p>Tax: {tax.toFixed(2)}</p>
--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -1,37 +1,79 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [displaySearched, setDisplaySearched] = useState([]);
     const [cart, setCart] = useState([]);
     useEffect(() => {
         fetch('./products.JSON')
             .then(res => res.json())
-            .then(data => setProducts(data))
+            .then(data => {
+                setProducts(data)
+                setDisplaySearched(data);
+            })
     }, [])
 
+    useEffect(() => {
+        if (products.length) {
+            //call the local storage data
+            const savedCart = getStoredCart();
+            //make new array which are present in local storage
+            const storedcart = [];
+            for (const key in savedCart) {
+                // console.log(savedCart[key]);
+                //find out out the local storage data in main db
+                const find = products.find(product => product.key === key);
+                if (find) {
+                    const quantity = savedCart[key];
+                    find.quantity = quantity;
+                    storedcart.push(find);
+                }
+            }
+            //for showing cart ui localstore cart item
+            setCart(storedcart);
+        }
+    }, [products])
     const handleAddToCart = ((product) => {
+        console.log(...cart);
         const newCart = [...cart, product];
         setCart(newCart);
+        // save to local storage
+        addToDb(product.key);
     })
+
+    const handleSeacrh = event => {
+        const searchText = event.target.value;
+        const matchedProducts = products.filter(product => product.name.toLowerCase().includes(searchText.toLowerCase()));
+        setDisplaySearched(matchedProducts);
+        console.log(displaySearched.length);
+
+
+    }
     return (
-        <div className='shop-container'>
-            <div className="product-container">
-                {/* <h2>Products: {products.length}</h2> */}
-                {
-                    products.map(product => <Product
-                        product={product}
-                        key={product.key}
-                        handleAddToCart={handleAddToCart}
-                    >
-                    </Product>)
-                }
+        <section>
+            <div className="seacrh-container">
+                <input onChange={handleSeacrh} type="text" placeholder='item name' />
             </div>
-            <div className="cart-summary">
-                <Cart cart={cart}></Cart>
+            <div className='shop-container'>
+                <div className="product-container">
+                    {/* <h2>Products: {products.length}</h2> */}
+                    {
+                        displaySearched.map(product => <Product
+                            product={product}
+                            key={product.key}
+                            handleAddToCart={handleAddToCart}
+                        >
+                        </Product>)
+                    }
+                </div>
+                <div className="cart-summary">
+                    <Cart cart={cart}></Cart>
+                </div>
             </div>
-        </div>
+        </section>
     );
 };
 
